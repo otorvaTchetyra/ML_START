5c7d144afeaf3eb534613392d0da0b8513aa588b
@royal_nuriev <royalnuriev572@gmail.com>
2026-04-28 22:37:44 +0300

    Проверено подключение к БД

--- Client/Client/ViewModels/MainWindowViewModel.cs
+++ Client/Client/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,24 @@
-﻿namespace Client.ViewModels
+﻿using Client.Models;
+using System;
+
+namespace Client.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting { get; } = "Welcome to Avalonia!";
+        public string Greeting { get; set; }
+
+        public MainWindowViewModel()
+        {
+            try
+            {
+                using var db = new AppDbContext();
+                var canConnect = db.Database.CanConnect();
+                Greeting = canConnect ? "БД подключена!" : "Ошибка подключения";
+            }
+            catch (Exception ex)
+            {
+                Greeting = $"Ошибка: {ex.Message}";
+            }
+        }
     }
 }
--- Client/Client/obj/Debug/net8.0/Client.AssemblyInfo.cs
+++ Client/Client/obj/Debug/net8.0/Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57df5d12dc1e69891396284a9461897dfb52a793")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65d543309383461d87a2ccd43843fc87a3c6a486")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
